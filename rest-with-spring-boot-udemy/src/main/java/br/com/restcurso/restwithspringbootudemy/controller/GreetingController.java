18550bad13bc08e7dd8a913518a59afd1218148f
GabrielWire <gabriel_wire@hotmail.com>
2020-06-16 16:42:29 -0300

    primeiro commit

---
+++ rest-with-spring-boot-udemy/src/main/java/br/com/restcurso/restwithspringbootudemy/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package br.com.restcurso.restwithspringbootudemy.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.restcurso.restwithspringbootudemy.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hellow, %s!";
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
